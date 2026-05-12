71d45f2d9cb0e2d40bb328bb0b544f79600825b3
Mohammed Habis <moh.habis@outlook.com>
2026-05-12 17:14:55 +0300

    .gitignore update

--- Backend/main/Database.js
+++ Backend/main/Database.js
@@ -1,11 +1,11 @@
-// Database.js
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Camaro@2010',
-  database: 'cement_company',
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
